55a3ca477560e0f49e24ee42fe015514eae5ee60
JeannetteFigueroa <108019168+JeannetteFigueroa@users.noreply.github.com>
2022-10-07 14:32:09 -0300

    ok

---
+++ Java/Jeannette_Figueroa_JSPrediccion.js
@@ -0,0 +1,28 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+//El console.log imprime Nací en 1980
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }copy
+/*El console.log imprime Nací en...  y colocará el valor 
+de la "variable" EntradaAñoNacimiento la cual decimos que es 1980
+EntradaAñoNacimiento = 1980 --> al final diría Nací en 1980 */
+
+function add(num1, num2){
+            console.log("¡Sumando números!");
+            console.log("num1 is: " + num1);
+            console.log("num2 is: " + num2);
+            var sum = num1 + num2;
+            console.log(sum);
+        }
+ /*Console.log imprimiria las siguientes cosas: 
+ 1- ¡Sumando números!
+ 2- Num1 is: 10
+ 3- Num2 is: 20
+ 4- 30
+ ya que si tomamos en cuenta que num1=10 y num2= 20 y luego estos 
+ los sumamos, la suma final sería 30*/      
+        
+    
